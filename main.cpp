582902c9ad23c822fb86a1fba94ee18f853f5caf
Ren Xin <renxin714@outlook.com>
2021-05-24 21:37:08 +0800

    mini changes

--- main.cpp
+++ main.cpp
@@ -5,11 +5,6 @@
 #include <iostream>
 
 int main(int argc, char *argv[]) {
-    if (argc > 1)
-        std::cout << argv[1] << std::endl;
     std::cout << "Hello, World!" << std::endl;
-    std::cout << "abc" << std::endl;
-    std::cout << "def" << std::endl;
-    std::cout << "gkadsjgkldsjgiaelj" << std::endl;
     return 0;
 }
